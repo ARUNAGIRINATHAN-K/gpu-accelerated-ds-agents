2c9113e1a55841732c203eefa9f103bc1c5f9afd
ARUNAGIRINATHAN_K <167340745+ARUNAGIRINATHAN-K@users.noreply.github.com>
2025-09-11 21:12:33 +0530

    Delete C directory

--- C/Exponential_Search.c
+++
@@ -1,8 +0,0 @@
-int exponentialSearch(int arr[], int n, int key) {
-    if (arr[0] == key)
-        return 0;
-    int i = 1;
-    while (i < n && arr[i] <= key)
-        i *= 2;
-    return binarySearch(arr, i/2, min(i, n-1), key);
-}
