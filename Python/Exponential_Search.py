82462b40e5ddb370c3857d5b0423bb712027154b
ARUNAGIRINATHAN_K <167340745+ARUNAGIRINATHAN-K@users.noreply.github.com>
2025-09-11 21:12:42 +0530

    Delete Python directory

--- Python/Exponential_Search.py
+++
@@ -1,8 +0,0 @@
-def exponential_search(arr, key):
-    if arr[0] == key:
-        return 0
-    n = len(arr)
-    i = 1
-    while i < n and arr[i] <= key:
-        i *= 2
-    return binary_search(arr[min(i, n-1):i//2-1:-1], key)
